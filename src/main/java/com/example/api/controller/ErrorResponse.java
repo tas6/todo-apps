f4a9a5aa8182cdd5aef051647b5f1d24abed7a93
Masaya Tashiro <tashiro256.bean@gmail.com>
2016-11-23 21:04:08 +0900

    メッセージ日本語化関連の処理を削除

--- src/main/java/com/example/api/controller/ErrorResponse.java
+++ src/main/java/com/example/api/controller/ErrorResponse.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ErrorResponse {
-  String error;
   String message;
   List<FieldErrorResponse> fieldErrors;
 
