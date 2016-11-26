87fc41c53a11a51cbb23fa080eadfb7d1f02fd7a
Masaya Tashiro <tashiro256.bean@gmail.com>
2016-11-26 11:51:22 +0900

    war対応

--- src/main/java/com/example/api/App.java
+++ src/main/java/com/example/api/App.java
@@ -2,12 +2,19 @@ package com.example.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(App.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    return application.sources(App.class);
   }
 
 }
